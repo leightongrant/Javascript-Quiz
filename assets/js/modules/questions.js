f73dd52634f756671d64c456e19396ac2f7614cd
Leighton Grant <oneal.grant@outlook.com>
2023-01-11 13:21:28 +0000

    adds questions array to questions.js

---
+++ assets/js/modules/questions.js
@@ -0,0 +1,6 @@
+const questions = [
+    ['q1', "Inside which HTML element do we put the JavaScript?", ["scripting", "script", "js", "javascript"]],
+    ['q2', "Where is the correct place to insert a JavaScript?", ["The body section", "The head section", "Neither", "Both"]]
+];
+
+export { questions };
