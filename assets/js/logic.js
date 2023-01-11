8002f123fa2db611635b852325c43c5e8c48ef08
Leighton Grant <oneal.grant@outlook.com>
2023-01-11 13:59:19 +0000

    adds a quiz object with some properties and a startTimer method

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,26 @@
+// Imports
 import { questions } from "./modules/questions.js";
+
+// Quiz object
+const quiz = {
+    questions: questions,
+    duration: 10,
+    question: 0,
+    score: 0,
+    startTimer () {
+        let timeDisplay = document.querySelector('#time');
+
+        const countdown = () => {
+            timeDisplay.innerText = this.duration;
+            this.duration -= 1;
+            if (this.duration < 0) {
+                clearInterval(clearTimer);
+            }
+        };
+        let clearTimer = setInterval(countdown, 1000);
+    }
+};
+
+
+console.log(quiz.startTimer());
+
